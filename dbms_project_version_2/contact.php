71bd2ad2e2484c5746b973c31d67b9b39e8effe5
VasaSrinivas <87537402+VasaSrinivas@users.noreply.github.com>
2021-10-07 17:19:08 +0530

    Add files via upload

---
+++ dbms_project_version_2/contact.php
@@ -0,0 +1,22 @@
+<!-- contact us form-->
+<div class="container">
+  <div class="row">
+    <div class="col-12 center">
+      <h1 style="padding-bottom: 20px;">Contact Us</h1>
+    </div>
+  </div>
+  <div class="col">
+    <form action="" method="post">
+      <input type="text" name="name" class="form-control" placeholder="Name"><br>
+      <input type="text" name="Course_Name" class="form-control" placeholder="Course name"><br>
+      <input type="text" name="Email" class="form-control" placeholder="Email"><br>
+      <textarea type="text" name="Message" class="form-control" placeholder="Issue" style="height: 150px;"></textarea><br>
+      <div class="row">
+        <div class="col-12 center">
+          <input type="submit" class="btn btn-primary" value="Send" name="submit">
+          <br><br>
+        </div>
+      </div>
+    </form>
+  </div>
+</div>
---
+++ dbms_project_version_2/courses.php
@@ -0,0 +1,142 @@
+<?php 
+  include('./main_include/header.php');
+?>
+<!--start course page banner-->
+<div class="container-fluid bg-dark">
+  <div class="row">
+    <img src="./images/coursebanner.jpg" alt="courses" style="height: 500px; width: 100% object-fit: cover;box-shadow: 10px;">
+  </div>
+</div>
+<!--end course page banner-->
+<!-- popular courses 1-->
+<div class="container">
+  <div>
+    <div class="row">
+      <div class="col-12 center">
+        <h1 style="padding-top: 20px;">All Courses</h1>
+      </div>
+    </div>
+  </div>
+  
+  <div class="row">
+    <div class="col-4">
+      <a href="#" class="btn" style="text-align: left; padding: 0px; margin: 0px;">
+        <div class="card">
+          <img src="images/course_demo.jpg" class="card-img-top" alt="course intro pic">
+          <div class="card-body">
+            <h5 class="card-title">Guitar Course for Beginners</h5>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+        
+     
+          <div class="card-footer">
+            <p class="card-text d-inline">Price: <small><del>&#8377 2000</del></small><span class="font-weight-bolder">&#8377 200</span>
+            </p>
+            <a href="coursedetails.php" class="btn btn-primary text-white font-weight-bolder float-right"> Enroll</a>
+          </div>
+        </div>
+      </a>
+    </div>
+    <div class="col-4">
+      <a href="#" class="btn" style="text-align: left; padding: 0px; margin: 0px;">
+        <div class="card">
+          <img src="images/course_demo.jpg" class="card-img-top" alt="course intro pic">
+          <div class="card-body">
+            <h5 class="card-title">Guitar Course for Beginners</h5>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+        
+     
+          <div class="card-footer">
+            <p class="card-text d-inline">Price: <small><del>&#8377 2000</del></small><span class="font-weight-bolder">&#8377 200</span>
+            </p>
+            <a href="#" class="btn btn-primary text-white font-weight-bolder float-right"> Enroll</a>
+          </div>
+        </div>
+      </a>
+    </div>
+    <div class="col-4">
+      <a href="#" class="btn" style="text-align: left; padding: 0px; margin: 0px;">
+        <div class="card">
+          <img src="images/course_demo.jpg" class="card-img-top" alt="course intro pic">
+          <div class="card-body">
+            <h5 class="card-title">Guitar Course for Beginners</h5>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+        
+     
+          <div class="card-footer">
+            <p class="card-text d-inline">Price: <small><del>&#8377 2000</del></small><span class="font-weight-bolder">&#8377 200</span>
+            </p>
+            <a href="#" class="btn btn-primary text-white font-weight-bolder float-right"> Enroll</a>
+          </div>
+        </div>
+      </a>
+    </div>
+    <div class="col-4">
+      <a href="#" class="btn" style="text-align: left; padding: 0px; margin: 0px;">
+        <div class="card">
+          <img src="images/course_demo.jpg" class="card-img-top" alt="course intro pic">
+          <div class="card-body">
+            <h5 class="card-title">Guitar Course for Beginners</h5>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+        
+     
+          <div class="card-footer">
+            <p class="card-text d-inline">Price: <small><del>&#8377 2000</del></small><span class="font-weight-bolder">&#8377 200</span>
+            </p>
+            <a href="#" class="btn btn-primary text-white font-weight-bolder float-right"> Enroll</a>
+          </div>
+        </div>
+      </a>
+    </div>
+    <div class="col-4">
+      <a href="#" class="btn" style="text-align: left; padding: 0px; margin: 0px;">
+        <div class="card">
+          <img src="images/course_demo.jpg" class="card-img-top" alt="course intro pic">
+          <div class="card-body">
+            <h5 class="card-title">Guitar Course for Beginners</h5>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+        
+     
+          <div class="card-footer">
+            <p class="card-text d-inline">Price: <small><del>&#8377 2000</del></small><span class="font-weight-bolder">&#8377 200</span>
+            </p>
+            <a href="#" class="btn btn-primary text-white font-weight-bolder float-right"> Enroll</a>
+          </div>
+        </div>
+      </a>
+    </div>
+    <div class="col-4">
+      <a href="#" class="btn" style="text-align: left; padding: 0px; margin: 0px;">
+        <div class="card">
+          <img src="images/course_demo.jpg" class="card-img-top" alt="course intro pic">
+          <div class="card-body">
+            <h5 class="card-title">Guitar Course for Beginners</h5>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+        
+     
+          <div class="card-footer">
+            <p class="card-text d-inline">Price: <small><del>&#8377 2000</del></small><span class="font-weight-bolder">&#8377 200</span>
+            </p>
+            <a href="#" class="btn btn-primary text-white font-weight-bolder float-right"> Enroll</a>
+          </div>
+        </div>
+      </a>
+    </div>
+    
+  </div>
+</div>
+<!-- popular courses 1-->
+<?php
+include('./main_include/footer.php');
+?>
